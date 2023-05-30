ef6e44bd5e68bca99f77478f004060269fd46a10
wingck <ckwwingo@gmail.com>
2023-05-29 22:27:08 -0400

    add T-sql

---
+++ dbo.Items.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Items] (
+    [ItemID]   INT            IDENTITY (1, 1) NOT NULL,
+    [ItemName] NVARCHAR (MAX) NULL,
+    [ItemNum]  INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Items] PRIMARY KEY CLUSTERED ([ItemID] ASC)
+);
+GO
---
+++ dbo.Purchases.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Purchases] (
+    [PurchaseID]  INT            IDENTITY (1, 1) NOT NULL,
+    [PurchaseNum] NVARCHAR (MAX) NULL,
+    [ItemID]      INT            DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_dbo.Purchases] PRIMARY KEY CLUSTERED ([PurchaseID] ASC),
+    CONSTRAINT [FK_dbo.Purchases_dbo.Items_ItemID] FOREIGN KEY ([ItemID]) REFERENCES [dbo].[Items] ([ItemID]) ON DELETE CASCADE
+);
+GO
+
+-- Create the non-clustered index
+CREATE NONCLUSTERED INDEX [IX_ItemID] ON [dbo].[Purchases]([ItemID] ASC);
+GO
+
+-- Create the trigger on the Purchases table
+GO
+CREATE TRIGGER trg_Purchases_AfterInsert ON Purchases
+AFTER INSERT
+AS
+BEGIN
+    -- Update the ItemNum in the Items table
+    UPDATE Items
+    SET ItemNum = ItemNum - (SELECT PurchaseNum FROM inserted WHERE Items.ItemID = inserted.ItemID)
+    FROM Items
+    INNER JOIN inserted ON Items.ItemID = inserted.ItemID
+END;
